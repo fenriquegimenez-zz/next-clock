a5b52b9f6b9023db1904a36ebdc566161af6d24b
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-10-11 14:50:04 -0300

    content aligned vertically to the center

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,17 @@ import Head from "next/head"
 
 const index = () => {
   return (
-    <div className="container">
+    <div className="h-screen flex flex-wrap content-center justify-center">
       <Head>
         <title>Next Clock</title>
       </Head>
-      <h1 className="text-center mx-auto my-auto py-3">
-        Welcome to this Next JS Clock!
-      </h1>
-      <Fecha />
-      <Hora />
+      <div>
+        <h1 className="text-center mx-auto my-auto py-3">
+          Welcome to this Next JS Clock!
+        </h1>
+        <Fecha />
+        <Hora />
+      </div>
     </div>
   )
 }
