2d4b98aaa2bd533a67001d7d917d7f3476795026
Enrique Gimenez <fenriquegimenez@gmail.com>
2021-10-11 14:31:18 -0300

    favicon and page title modified

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
 import React from "react"
 import Fecha from "../components/date"
 import Hora from "../components/hour"
+import Head from "next/head"
 
 const index = () => {
   return (
     <div className="container">
+      <Head>
+        <title>Next Clock</title>
+      </Head>
       <h1 className="text-center mx-auto my-auto py-3">
         Welcome to this Next JS Clock!
       </h1>
